b0ea67115603f3227112030fa68b9bf53e2ef750
sebdotv <sebdotv@gmail.com>
2019-12-01 22:53:12 +0100

    change p2 to tailrec

--- src/main/scala/d01/Part2.scala
+++ src/main/scala/d01/Part2.scala
@@ -1,18 +1,24 @@
 package d01
 
 import java.nio.file.{Files, Paths}
+
+import scala.annotation.tailrec
 import scala.collection.JavaConverters._
 
 object Part2 extends App {
   val lines = Files.readAllLines(Paths.get("input/01.txt")).asScala
 
-  def totalFuel(m: Long): Long =
-    m match {
-      case 0L => 0L
-      case _ =>
-        val result = math.max(0L, m / 3 - 2)
-        result + totalFuel(result)
-    }
+  def totalFuel(m: Long) = {
+    @tailrec
+    def it(m: Long, acc: Long): Long =
+      m match {
+        case 0L => acc
+        case _ =>
+          val fuel = math.max(0L, m / 3 - 2)
+          it(fuel, acc + fuel)
+      }
+    it(m, 0)
+  }
 
   println(lines.map(_.toLong).map(totalFuel).sum)
 }
